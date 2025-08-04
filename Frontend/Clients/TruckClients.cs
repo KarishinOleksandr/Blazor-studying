fe6ed0d450098a7a762aff76a2b48569c3b91cd0
Oleksandr <skarishin12345@gmail.com>
2025-08-04 14:12:09 +0300

    basic

---
+++ Frontend/Clients/TruckClients.cs
@@ -0,0 +1,49 @@
+﻿using Frontend.Models;
+
+namespace Frontend.Clients
+{
+    public class TruckClients
+    {
+        private static readonly List<AllTruck> truckslist =
+        [
+            new()
+                {
+                    Id = 1,
+                    Model = "1",
+                    Brand = "Mercedes",
+                    maxSpeed = 120,
+                    maxLiftingCapacity = 40,
+                    Price = 40000,
+                    ReleaseDate = new DateOnly(12,09,25)
+                },
+
+                new()
+                {
+                    Id = 2,
+                    Model = "2",
+                    Brand = "Mercedes",
+                    maxSpeed = 150,
+                    maxLiftingCapacity = 40,
+                    Price = 45000,
+                    ReleaseDate = new DateOnly(13,09,25)
+                },
+
+                new()
+                {
+                    Id = 3,
+                    Model = "3",
+                    Brand = "Mercedes",
+                    maxSpeed = 160,
+                    maxLiftingCapacity = 40,
+                    Price = 50000,
+                    ReleaseDate = new DateOnly(15,09,25)
+                }
+        ];
+
+        public static Task<List<AllTruck>> GetAllTrucksAsync()
+        {
+            return Task.FromResult(truckslist.ToList());
+        }
+    }
+}
+    
---
+++ Frontend/Models/AllTruck.cs
@@ -0,0 +1,19 @@
+﻿namespace Frontend.Models
+{
+    public class AllTruck
+    {
+        public int Id { get; set; }
+
+        public required string Model { get; set; }
+
+        public required string Brand { get; set; }
+
+        public int maxSpeed { get; set; }
+
+        public int maxLiftingCapacity { get; set; }
+
+        public int Price { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+    }
+}
